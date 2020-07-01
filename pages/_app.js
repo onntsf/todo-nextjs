fa2655d1f94ee843588b63177057eb8aca3ec4fd
onntsf <onntsf@gmail.com>
2020-07-01 14:32:30 +0900

    delete unnecessary file

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,4 @@
 import '../styles/index.css'
-import '../styles/app.css'
-import '../styles/base.css'
 
 export default function IndexPage({ Component, pageProps }) {
   return <Component {...pageProps} />
